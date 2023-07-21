09bc45d370b84336211a38e79f6b6626961cfe64
rawsample <paul.olivier@eurecom.fr>
2023-07-21 15:20:35 +0200

    correct DecodeExit proc macro to get the type inside the option

--- sysforward/decode_derive/src/lib.rs
+++ sysforward/decode_derive/src/lib.rs
@@ -7,12 +7,51 @@ use syn;
 pub fn decode_exit_derive(input: TokenStream) -> TokenStream
 {
     let ast: syn::DeriveInput = syn::parse(input).unwrap();
+
+    // The name of the struct
     let name = ast.ident;
+    
+    // Extract the list of structure fields
+    let fields = match ast.data {
+        syn::Data::Struct(data_struct) => {
+            match data_struct.fields {
+                syn::Fields::Named(fields_named) => fields_named.named,
+                _ => panic!("Expected named fields in struct"),
+            }
+        },
+        _ => panic!("Expected struct"),
+    };
+
+    // Get the retval attribute
+    let retval_field = fields.iter()
+        .find(|field| field.ident.as_ref().map_or(false, |ident| ident == "retval"))
+        .expect("No field retval in the struct");
+
+    // Get the type inside the Option<>
+    let inner_type = if let syn::Type::Path(path) = &retval_field.ty {
+        if path.path.segments.len() == 1 && path.path.segments[0].ident == "Option" {
+            let args = &path.path.segments[0].arguments;
+            if let syn::PathArguments::AngleBracketed(angle_bracketed) = args {
+                if let syn::GenericArgument::Type(ty) = &angle_bracketed.args[0] {
+                    Some(ty.clone())
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    } else {
+        None
+    };
+
     let gen = quote! {
         impl DecodeExit for #name {
-            fn decode_exit(&mut self, pid: i32, operation: &Box<Operation>) -> Result<(), std::io::Error>
+            fn decode_exit(&mut self, value: usize, pid: i32, operation: &Box<Operation>) -> Result<(), std::io::Error>
             {
-                self.retval.as_mut().unwrap().decode(pid, operation);
+                self.retval.as_mut() = Some( #inner_type::from(value) );
                 Ok(())
             }
         }
